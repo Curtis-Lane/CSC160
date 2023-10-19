ddc99a350d792a77b31d9b0bafaff660be7e73ce
Curtis-Lane <129118489+Curtis-Lane@users.noreply.github.com>
2023-10-19 10:25:07 -0600

    Start work on the Grab the Treasure assignment

---
+++ GrabTheTreasure/Board.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+namespace GrabTheTreasure {
+	public class Board {
+		private char[][] board = new char[12][]; /* {
+			{' ', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', ' '},
+			{'|', ' ', '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'},
+			{'|', ' ', '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'},
+			{'|', ' ', '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'P', 'E'},
+			{'|', ' ', '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'},
+			{'|', ' ', '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'},
+			{'|', ' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' ', '|'},
+			{'|', ' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' ', '|'},
+			{'|', ' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' ', '|'},
+			{'|', ' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' ', '|'},
+			{'|', ' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' ', '|'},
+			{'|', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '|'}
+		}; */
+
+		public Board() {
+			board[0] = new char[12]  { ' ', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', ' '};
+			board[1] = new char[12]  { '|', ' ', '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|' };
+			board[2] = new char[12]  { '|', ' ', '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|' };
+			board[3] = new char[12]  { '|', ' ', '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'E' };
+			board[4] = new char[12]  { '|', ' ', '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|' };
+			board[5] = new char[12]  { '|', ' ', '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|' };
+			board[6] = new char[12]  { '|', ' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' ', '|' };
+			board[7] = new char[12]  { '|', ' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' ', '|' };
+			board[8] = new char[12]  { '|', ' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' ', '|' };
+			board[9] = new char[12]  { '|', ' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' ', '|' };
+			board[10] = new char[12] { '|', ' ', 'O', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' ', '|' };
+			board[11] = new char[12] { '|', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '|' };
+		}
+
+		public void Draw(int[] playerPos) {
+			// Going to have to get the pos of entities, unless I store them here
+			char[][] tempBoard = new char[12][];// = board;
+
+			for(int i = 0; i < board.GetLength(0); i++) {
+				tempBoard[i] = new char[12];
+				board[i].CopyTo(tempBoard[i], 0);
+			}
+
+			tempBoard[playerPos[0]][playerPos[1]] = 'P';
+
+			Console.Clear();
+			for(int row = 0; row < board.GetLength(0); row++) {
+				for(int column = 0; column < board[row].GetLength(0); column++) {
+					Console.Write(tempBoard[row][column]);
+				}
+				Console.Write('\n');
+			}
+		}
+
+		public bool IsValidMove(int[] newPlayerPos) {
+			if(board[newPlayerPos[0]][newPlayerPos[1]] == '|' || board[newPlayerPos[0]][newPlayerPos[1]] == '_') {
+				return false;
+			}
+
+			return true;
+		}
+	}
+}
---
+++ GrabTheTreasure/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading;
+
+namespace GrabTheTreasure {
+	class Program {
+		static void Main(string[] args) {
+			Console.Title = "Grab the Treasure!";
+
+			Board board = new Board();
+			int[] playerPos = new int[2] {3, 10};
+
+			bool done = false;
+			while(!done) {
+				board.Draw(playerPos);
+				if(Console.KeyAvailable) {
+					ConsoleKeyInfo key = Console.ReadKey(true);
+
+					switch(key.Key) {
+						case ConsoleKey.LeftArrow:
+							//Console.WriteLine("Left");
+							if(board.IsValidMove(new int[2] {playerPos[0], playerPos[1] - 1})) {
+								playerPos[1] -= 1;
+							}
+							break;
+						case ConsoleKey.RightArrow:
+							//Console.WriteLine("Right");
+							if(board.IsValidMove(new int[2] {playerPos[0], playerPos[1] + 1})) {
+								playerPos[1] += 1;
+							}
+							break;
+						case ConsoleKey.UpArrow:
+							//Console.WriteLine("Up");
+							if(board.IsValidMove(new int[2] {playerPos[0] - 1, playerPos[1]})) {
+								playerPos[0] -= 1;
+							}
+							break;
+						case ConsoleKey.DownArrow:
+							//Console.WriteLine("Down");
+							if(board.IsValidMove(new int[2] {playerPos[0] + 1, playerPos[1]})) {
+								playerPos[0] += 1;
+							}
+							break;
+						case ConsoleKey.Escape:
+							done = true;
+							break;
+						default:
+							break;
+					}
+				}
+
+				if(!done) {
+					Thread.Sleep(750);
+				}
+			}
+		}
+	}
+}
