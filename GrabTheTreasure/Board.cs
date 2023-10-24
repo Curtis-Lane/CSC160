ea9e89c5aeaa654532fad46f94822264af905e80
Curtis-Lane <129118489+Curtis-Lane@users.noreply.github.com>
2023-10-24 09:57:12 -0600

    Finish the Grab the Treasure assignment (I think)

--- GrabTheTreasure/Board.cs
+++ GrabTheTreasure/Board.cs
@@ -1,23 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+using System.Linq;
 
 namespace GrabTheTreasure {
 	public class Board {
-		private char[][] board = new char[12][]; /* {
+		private char[][] board = new char[13][];
+		private char[] wallChars = {'|', '_'};
+		private int[] enemyPos = {6, 1};
+		private int[] exitPos = {3, 11};
+		private int[] treasurePos = {10, 2};
+		private bool grabbedTreasure = false;
+		/* {
 			{' ', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', ' '},
 			{'|', ' ', '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'},
 			{'|', ' ', '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'},
-			{'|', ' ', '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'P', 'E'},
+			{'|', ' ', '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'P', 'D'},
 			{'|', ' ', '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'},
 			{'|', ' ', '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'},
+			{'|', 'E', ' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' ', '|'},
 			{'|', ' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' ', '|'},
 			{'|', ' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' ', '|'},
 			{'|', ' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' ', '|'},
-			{'|', ' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' ', '|'},
-			{'|', ' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' ', '|'},
+			{'|', ' ', 'O', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' ', '|'},
+			{'|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'},
 			{'|', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '|'}
 		}; */
 
@@ -25,29 +30,35 @@ namespace GrabTheTreasure {
 			board[0] = new char[12]  { ' ', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', ' '};
 			board[1] = new char[12]  { '|', ' ', '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|' };
 			board[2] = new char[12]  { '|', ' ', '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|' };
-			board[3] = new char[12]  { '|', ' ', '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'E' };
+			board[3] = new char[12]  { '|', ' ', '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
 			board[4] = new char[12]  { '|', ' ', '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|' };
 			board[5] = new char[12]  { '|', ' ', '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|' };
 			board[6] = new char[12]  { '|', ' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' ', '|' };
 			board[7] = new char[12]  { '|', ' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' ', '|' };
 			board[8] = new char[12]  { '|', ' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' ', '|' };
 			board[9] = new char[12]  { '|', ' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' ', '|' };
-			board[10] = new char[12] { '|', ' ', 'O', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' ', '|' };
-			board[11] = new char[12] { '|', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '|' };
+			board[10] = new char[12] { '|', ' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' ', '|' };
+			board[11] = new char[12] { '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|' };
+			board[12] = new char[12] { '|', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '|' };
 		}
 
 		public void Draw(int[] playerPos) {
-			// Going to have to get the pos of entities, unless I store them here
-			char[][] tempBoard = new char[12][];// = board;
+			char[][] tempBoard = new char[board.Length][];
 
-			for(int i = 0; i < board.GetLength(0); i++) {
-				tempBoard[i] = new char[12];
+			for(int i = 0; i < board.Length; i++) {
+				tempBoard[i] = new char[board[i].Length];
 				board[i].CopyTo(tempBoard[i], 0);
 			}
 
-			tempBoard[playerPos[0]][playerPos[1]] = 'P';
+			tempBoard[exitPos[0]][exitPos[1]] = 'D';
+			if(!grabbedTreasure) {
+				tempBoard[treasurePos[0]][treasurePos[1]] = 'O';
+			}
 
-			Console.Clear();
+			tempBoard[playerPos[0]][playerPos[1]] = 'P';
+			tempBoard[enemyPos[0]][enemyPos[1]] = 'E';
+
+			//Console.Clear();
 			for(int row = 0; row < board.GetLength(0); row++) {
 				for(int column = 0; column < board[row].GetLength(0); column++) {
 					Console.Write(tempBoard[row][column]);
@@ -56,12 +67,68 @@ namespace GrabTheTreasure {
 			}
 		}
 
-		public bool IsValidMove(int[] newPlayerPos) {
-			if(board[newPlayerPos[0]][newPlayerPos[1]] == '|' || board[newPlayerPos[0]][newPlayerPos[1]] == '_') {
+		public bool IsValidMove(int[] newPos) {
+			if(newPos[0] < 0 || newPos[0] > board.Length || newPos[1] < 0 || newPos[1] > board[0].Length) {
+				return false;
+			} else if(wallChars.Contains(board[newPos[0]][newPos[1]])) {
 				return false;
 			}
 
 			return true;
+		}
+
+		public void MoveEnemy() {
+			int direction = new Random().Next(0, 4);
+			switch(direction) {
+				// Up
+				case 0:
+					if(IsValidMove(new int[] {enemyPos[0] - 1, enemyPos[1]})) {
+						enemyPos[0] -= 1;
+					}
+					break;
+				// Right
+				case 1:
+					if(IsValidMove(new int[] {enemyPos[0], enemyPos[1] + 1})) {
+						enemyPos[1] += 1;
+					}
+					break;
+				// Down
+				case 2:
+                    if(IsValidMove(new int[] {enemyPos[0] + 1, enemyPos[1]})) {
+                        enemyPos[0] += 1;
+                    }
+                    break;
+				// Left
+				case 3:
+                    if(IsValidMove(new int[] {enemyPos[0], enemyPos[1] - 1})) {
+                        enemyPos[1] -= 1;
+                    }
+                    break;
+			}
+		}
+
+		public bool HasEnemyTouchedPlayer(int[] playerPos) {
+			if(playerPos[0] == enemyPos[0] && playerPos[1] == enemyPos[1]) {
+				return true;
+			}
+
+			return false;
+		}
+
+		public void HasPlayerGrabbedTreasure(int[] playerPos) {
+			if(!grabbedTreasure) {
+				if(playerPos[0] == treasurePos[0] && playerPos[1] == treasurePos[1]) {
+					grabbedTreasure = true;
+				}
+			}
+		}
+
+		public bool HasPlayerWon(int[] playerPos) {
+			if(grabbedTreasure && (playerPos[0] == exitPos[0] && playerPos[1] == exitPos[1])) {
+				return true;
+			}
+
+			return false;
 		}
 	}
 }
--- GrabTheTreasure/Program.cs
+++ GrabTheTreasure/Program.cs
@@ -7,33 +7,52 @@ namespace GrabTheTreasure {
 			Console.Title = "Grab the Treasure!";
 
 			Board board = new Board();
-			int[] playerPos = new int[2] {3, 10};
+
+			string exitState = "";
+
+			int[] playerPos = new int[2] {3, 11};
+			string? playerName = null;
+
+			Console.WriteLine("What is your name, mighty adventurer?");
+			bool valid = false;
+			while(!valid) {
+				playerName = Console.ReadLine();
+				if(playerName == null || playerName == "") {
+					Console.WriteLine("\n" + "Please enter a valid name!");
+				} else {
+					valid = true;
+				}
+			}
+			Console.WriteLine("\n" + "Welcome adventurer " + playerName + ", you must grab the treasure and get out as fast as you can!" + "\n");
 
 			bool done = false;
 			while(!done) {
-				board.Draw(playerPos);
 				if(Console.KeyAvailable) {
 					ConsoleKeyInfo key = Console.ReadKey(true);
 
 					switch(key.Key) {
+						case ConsoleKey.A:
 						case ConsoleKey.LeftArrow:
 							//Console.WriteLine("Left");
 							if(board.IsValidMove(new int[2] {playerPos[0], playerPos[1] - 1})) {
 								playerPos[1] -= 1;
 							}
 							break;
+						case ConsoleKey.D:
 						case ConsoleKey.RightArrow:
 							//Console.WriteLine("Right");
 							if(board.IsValidMove(new int[2] {playerPos[0], playerPos[1] + 1})) {
 								playerPos[1] += 1;
 							}
 							break;
+						case ConsoleKey.W:
 						case ConsoleKey.UpArrow:
 							//Console.WriteLine("Up");
 							if(board.IsValidMove(new int[2] {playerPos[0] - 1, playerPos[1]})) {
 								playerPos[0] -= 1;
 							}
 							break;
+						case ConsoleKey.S:
 						case ConsoleKey.DownArrow:
 							//Console.WriteLine("Down");
 							if(board.IsValidMove(new int[2] {playerPos[0] + 1, playerPos[1]})) {
@@ -47,11 +66,23 @@ namespace GrabTheTreasure {
 							break;
 					}
 				}
+				board.HasPlayerGrabbedTreasure(playerPos);
+				board.MoveEnemy();
+				board.Draw(playerPos);
+
+				if(board.HasEnemyTouchedPlayer(playerPos)) {
+					done = true;
+					exitState = "Unfortunately adventurer " + playerName + ", you have fallen in battle.";
+				} else if(board.HasPlayerWon(playerPos)) {
+					done = true;
+					exitState = "Great job adventurer " + playerName + ", you have collected the treasure and escaped successfully!";
+				}
 
 				if(!done) {
 					Thread.Sleep(750);
 				}
 			}
+			Console.WriteLine("\n" + exitState);
 		}
 	}
 }
